7f573261388ac3af6a90279d3d3c94e89d79de42
Jun <s14113136@stu.edu.tw>
2024-01-27 15:08:37 +0800

    Create input code system and prefab

--- Assets/Project/Scripts/Ingame/Controller/InputCodeController.cs
+++ Assets/Project/Scripts/Ingame/Controller/InputCodeController.cs
@@ -1,10 +1,11 @@
 using GGJ.Ingame.Common;
+using GGJ.Ingame.UI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-namespace GGJ.Ingame.UI
+namespace GGJ.Ingame.Controller
 {
     public class InputCodeController : MonoBehaviour
     {
@@ -12,34 +13,43 @@ namespace GGJ.Ingame.UI
         private int _nextCodeIndex;
 
         private IInputCodeSystem _inputCodeSystem;
+        private IInputCodePanel _inputCodePanel;
 
         private void Start ()
         {
             Initialize(
-                FindFirstObjectByType<InputCodeSystem>());
+                FindFirstObjectByType<InputCodeSystem>(),
+                FindFirstObjectByType<InputCodePanel>());
         }
 
-        private void Initialize(IInputCodeSystem inputCodeSystem)
+        private void Initialize(IInputCodeSystem inputCodeSystem, IInputCodePanel inputCodePanel)
         {
             _inputCodeSystem = inputCodeSystem;
+            _inputCodePanel = inputCodePanel;
 
             Assert.IsNotNull(_inputCodeSystem);
+            Assert.IsNotNull(_inputCodePanel);
 
             _nextArrangeCode = GenerateTestCode();
             Debug.Log($"Next code: {_nextArrangeCode}");
+
+            _inputCodePanel.ClearAll();
+            _inputCodePanel.SetInputCodes(_nextArrangeCode);
         }
 
         private void Update()
         {
-            UpdateInputCode();
+            UpdateCheckCode();
         }
 
-        public void UpdateInputCode()
+        public void UpdateCheckCode()
         {
             var inputCode = _inputCodeSystem.GetCurrentInputCode();
             if (inputCode.HasValue && _nextArrangeCode[_nextCodeIndex] == inputCode)
             {
+                _inputCodePanel.SetCompletedIndex(_nextCodeIndex);
                 _nextCodeIndex++;
+
                 var isCompleted = CheckInputComplete(_nextCodeIndex, _nextArrangeCode);
                 if (isCompleted)
                 {
@@ -60,6 +70,9 @@ namespace GGJ.Ingame.UI
             Debug.Log($"Completed code: {_nextArrangeCode}");
             _nextArrangeCode = GenerateTestCode();
             _nextCodeIndex = 0;
+
+            _inputCodePanel.ClearAll();
+            _inputCodePanel.SetInputCodes(_nextArrangeCode);
             Debug.Log($"Next code: {_nextArrangeCode}");
         }
 
@@ -68,11 +81,11 @@ namespace GGJ.Ingame.UI
             var simpleCodes = new string[]
             {
                 "aaabbb",
-                "abcdef",
+                "1111111",
+                "12345678",
                 "123abc",
             };
-            return simpleCodes[Random.Range(0, simpleCodes.Length - 1)];
+            return simpleCodes[Random.Range(0, simpleCodes.Length)];
         }
-
     }
 }
