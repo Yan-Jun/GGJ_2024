3270786cbb8fbd32f09390c617da5f1eb5cc2e68
Jun <s14113136@stu.edu.tw>
2024-01-28 13:35:46 +0800

    Create wall amount bar and prefabs

---
+++ Assets/Project/Scripts/Ingame/UI/WallAmountBar.cs
@@ -0,0 +1,81 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GGJ.Ingame.UI
+{
+    public class WallAmountBar : MonoBehaviour
+    {
+        [SerializeField] private WallAmountSetting _wallAmountSetting;
+        [SerializeField] private AmountBarSetting _amountBarSetting;
+
+        private int _maxAmount;
+        private int _currentAmount;
+
+        public int testAmount;
+
+        private void Start()
+        {
+            Initialize(5000, 5000);
+        }
+
+        private void Update()
+        {
+            UpdateWallAmount(testAmount);
+        }
+
+        public void UpdateWallAmount(int currentAmount)
+        {
+            _currentAmount = currentAmount;
+            var barCount = _currentAmount / _amountBarSetting.OneBarAmount;
+            var lastBarAmount = _currentAmount % _amountBarSetting.OneBarAmount;
+            for (var i = 0; i < _amountBarSetting.Images.Length; i++)
+            {
+                if (i < barCount)
+                {
+                    // Full
+                    _amountBarSetting.Images[i].sprite = 
+                        _wallAmountSetting.Walls.First((wall) => wall.Amount <= _amountBarSetting.OneBarAmount).WallSprite;
+                }
+                else if (i == barCount)
+                {
+                    // Half
+                    _amountBarSetting.Images[i].sprite = 
+                        _wallAmountSetting.Walls.First((wall) => wall.Amount <= lastBarAmount).WallSprite;
+                }
+                else
+                {
+                    // Empty
+                    _amountBarSetting.Images[i].sprite = 
+                        _wallAmountSetting.Walls.First((wall) => wall.Amount == 0).WallSprite;
+                }
+            }
+        }
+
+        public void Initialize(int currentAmount, int maxAmount)
+        {
+            _currentAmount = currentAmount;
+            _maxAmount = maxAmount;
+        }
+
+        [System.Serializable]
+        public struct AmountBarSetting
+        {
+            public int OneBarAmount;
+            public Image[] Images;
+        }
+
+        [System.Serializable]
+        public struct WallAmountSetting
+        {
+            public Wall[] Walls;
+        }
+
+        [System.Serializable]
+        public struct Wall
+        {
+            public float Amount;
+            public Sprite WallSprite;
+        }
+    }
+}
